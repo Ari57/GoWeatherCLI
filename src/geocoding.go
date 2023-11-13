c0cd165dfdd20a19e544908b5988a02fd7354b18
Nathan Devey-Skeete <njds1676@gmail.com>
2023-11-13 15:23:53 +0000

    Added geocoding file, and updated README

---
+++ src/geocoding.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Geocode(GeocodeKey string, location string) (string, string, error) {
+	HttpLink := "https://maps.googleapis.com/maps/api/geocode/json?key=" + GeocodeKey + "&address=" + location
+
+	resp, err := http.Get(HttpLink)
+	ErrorChecker(err)
+
+	responseData, err := io.ReadAll(resp.Body)
+	ErrorChecker(err)
+
+	type Result struct {
+		Geometry struct {
+			Location struct {
+				Lat float64 `json:"lat"`
+				Lng float64 `json:"lng"`
+			} `json:"location"`
+		} `json:"geometry"`
+	}
+
+	type Response struct {
+		Results []Result `json:"results"`
+	}
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	if len(responseObject.Results) > 0 {
+		lat := responseObject.Results[0].Geometry.Location.Lat
+		lng := responseObject.Results[0].Geometry.Location.Lng
+
+		return fmt.Sprint(lat), fmt.Sprint(lng), nil
+	}
+
+	return "0", "0", fmt.Errorf("No results found")
+
+}
+
+func ErrorChecker(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -15,18 +17,42 @@ func main() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
+	GeocodeKey := os.Getenv("GEOCODE_KEY")
 	ApiKey := os.Getenv("API_KEY")
 
-	response, err := http.Get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/3840?res=3hourly&key=" + ApiKey)
+	location := ""
+
+	fmt.Print("Enter a location: ")
+	fmt.Scan(&location)
+	fmt.Println("Provided location is", location)
+
+	lat, lng, err := Geocode(GeocodeKey, location)
+	ErrorCheck(err)
+
+	HttpLink := "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lng + "&appid=" + ApiKey + "&units=metric"
+
+	resp, err := http.Get(HttpLink)
+	ErrorChecker(err)
+
+	responseData, err := io.ReadAll(resp.Body)
+	ErrorChecker(err)
+
+	type CurrentWeather struct {
+		Temp float64 `json:"temp"`
+	}
+
+	type Response struct {
+		Current CurrentWeather `json:"current"`
+	}
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+	temperature := fmt.Sprint(responseObject.Current.Temp)
+	fmt.Println("The current temperature is " + temperature + " Celsius")
+}
+
+func ErrorCheck(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	sbE := string(body)
-	log.Printf(sbE)
 }
